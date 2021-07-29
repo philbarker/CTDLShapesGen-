0ffc806c21fde82f1417cb06585f39116c36b5d5
Phil Barker <phil.barker@pjjk.co.uk>
2021-07-29 17:04:30 +0100

    blacking

--- tests/test_apConverter.py
+++ tests/test_apConverter.py
@@ -1,6 +1,7 @@
 import pytest
 from CTDLAPProcs import APConverter, readJSONFile
 
+
 @pytest.fixture(scope="module")
 def converter():
     converter = APConverter()
@@ -12,6 +13,7 @@ def test_readJSONFile():
     assert len(ap_data["RawShapes"]) == 42
     assert ap_data["RawShapes"][0]["ClassURI"] == "ceterms:ApprenticeshipCertificate"
 
+
 def test_initAP(converter):
     assert converter
     assert converter.metadata == {}
@@ -19,13 +21,21 @@ def test_initAP(converter):
     assert converter.shapeInfo == {}
     assert converter.propertyStatements == []
 
+
 def test_load_JSON_AP(converter):
     converter.load_json_AP("InputData/policyBrowserData.json")
     shInfo = converter.shapeInfo
     assert len(shInfo) == 42
     assert shInfo["#ApprenticeshipCertificate"]["targetType"] == "sh:NodeShape"
-    assert shInfo["#ApprenticeshipCertificate"]["target"] == "ceterms:ApprenticeshipCertificate"
-    assert "ceterms:credentialStatusType" in shInfo["#ApprenticeshipCertificate"]["properties"]
+    assert (
+        shInfo["#ApprenticeshipCertificate"]["target"]
+        == "ceterms:ApprenticeshipCertificate"
+    )
+    assert (
+        "ceterms:credentialStatusType"
+        in shInfo["#ApprenticeshipCertificate"]["properties"]
+    )
+
 
 def test_load_namespaces(converter):
     converter.load_namespaces("InputData/namespaces.csv")
